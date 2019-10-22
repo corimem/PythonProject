6227b07ba9526968fb32089ac4673a33dbd0750a
corimem <55324978+corimem@users.noreply.github.com>
2019-10-22 12:29:47 -0400

    Update ReadWriteImage.py

--- ReadWriteImage.py
+++ ReadWriteImage.py
@@ -1,16 +1,26 @@
 #prompts user for image path and opens image in a seperate window. Closes 
 import cv2
+import numpy as np
+from smooth import smooth
+from rotate import rotate
+from grayscale import gray
+
 name=input('input name of image: ')
+do=input('What do you want to do to the image? Input R for rotate, G for grayscale or S for smooth:')
+
 img=cv2.imread(name)
-cv2.imshow('image', img)
+editimg=img.copy()
+
+C=(int(len(img[0])))
+R=(int(len(img)))
+
+if do == 'R':
+    editimg=rotate(R,C,img,editimg)
+if do == 'G':
+    editimg=gray(R,C,img,editimg)
+if do == 'S':
+    editimg=smooth(R,C,img,editimg)
+
+cv2.imshow('image',editimg)
 cv2.waitKey(0)
-cv2.destroyAllWindows()        
-
-degree=input('Enter how many degrees you want to rotate. Enter 90, 180, 270 or mirror: ')
-
-
-while degree.lower()!='mirror' and degree!='90' and degree!='180' and degree!='270':
-        degree=input('Enter how many degrees you want to rotate. Enter 90, 180, 270 or mirror: ')
-
-
-
+cv2.destroyAllWindows() 
